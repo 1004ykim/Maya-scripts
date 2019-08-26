c2f7d4762ee5b5174eaaba4d23b40f99ff79e284
githobhuy <ae2eo@hotmail.com>
2019-08-26 15:15:39 -0700

    Create MayaCamera_to_Unity.cs

---
+++ MayaCamera_to_Unity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camController : MonoBehaviour
+{
+    public Camera MainCam;
+    public GameObject camMove;
+    public Transform Target;
+
+
+
+    void LateUpdate()
+    {
+        transform.position = camMove.transform.position;
+       transform.LookAt(Target);
+//       MainCam.nearClipPlane = camMove.transform.localScale.x;
+//       MainCam.farClipPlane = camMove.transform.localScale.y;
+       MainCam.fieldOfView = camMove.transform.localScale.z - 2;
+
+
+    }
+
+}
